97b44e5de34f9845a7676a1be1827000d483a64a
xkurozaru <kurozaru.210@gmail.com>
2025-01-23 22:49:17 +0900

    style: 🎨 apply format

--- migration/env.py
+++ migration/env.py
@@ -1,12 +1,10 @@
 from logging.config import fileConfig
 
-#追加
-from models import Base 
-
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
+from sqlalchemy import engine_from_config, pool
+
+# 追加
+from models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -22,7 +20,7 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 
-#変更
+# 変更
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
@@ -69,9 +67,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
--- settings.py
+++ settings.py
@@ -1,13 +1,13 @@
 import os
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+
 from dotenv import load_dotenv
+from sqlalchemy import create_engine
 
 # .env ファイルから環境変数を読み込む
 load_dotenv()
 
 # PostgreSQL の接続情報を環境変数から取得
-DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL is not set in .env file.")
@@ -15,8 +15,5 @@ if not DATABASE_URL:
 # エンジンの作成（データベースに接続するためのエンジン）
 Engine = create_engine(
     DATABASE_URL,  # 接続するデータベースのURL
-    echo=False  # SQLの実行内容を表示しない
+    echo=False,  # SQLの実行内容を表示しない
 )
-
-# Base クラスを定義（モデルを定義するためのベースクラス）
-Base = declarative_base()
