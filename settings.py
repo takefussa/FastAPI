6543d9c27533d07a4afe270875fe999a9784d3d2
xkurozaru <kurozaru.210@gmail.com>
2025-01-23 23:06:08 +0900

    chore: 🔥 remove dotenv

--- settings.py
+++ settings.py
@@ -1,16 +1,12 @@
 import os
 
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
-
-# .env ファイルから環境変数を読み込む
-load_dotenv()
 
 # PostgreSQL の接続情報を環境変数から取得
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
-    raise ValueError("DATABASE_URL is not set in .env file.")
+    raise ValueError("DATABASE_URL is not set in environment variables.")
 
 # エンジンの作成（データベースに接続するためのエンジン）
 Engine = create_engine(
