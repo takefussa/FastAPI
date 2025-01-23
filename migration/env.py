f042885354a494d3e63171ade6c6d40ac4299753
xkurozaru <kurozaru.210@gmail.com>
2025-01-23 22:52:10 +0900

    refactor: ♻️ seal secret

--- migration/env.py
+++ migration/env.py
@@ -1,3 +1,4 @@
+import os
 from logging.config import fileConfig
 
 from alembic import context
@@ -9,6 +10,7 @@ from models import Base
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("DB_URL", os.environ.get("DATABASE_URL"))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
